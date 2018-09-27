a5cec51ed6447d44c837494fbd87f199beb9aaeb
oscarvago <dos.multimedium@gmail.com>
2018-09-26 20:59:28 -0500

    commit practica4

---
+++ Practica4/formulario.swift
@@ -0,0 +1,35 @@
+//
+//  formulario.swift
+//  Practica4
+//
+//  Created by MTI on 18/09/18.
+//  Copyright © 2018 MTI. All rights reserved.
+//
+
+import UIKit
+
+class formulario: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
